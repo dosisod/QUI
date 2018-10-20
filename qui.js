d94965ba2f66088911b3537f6c9b04cd497cc33c
dosisod <39638017+dosisod@users.noreply.github.com>
2018-10-19 23:38:06 -0700

    setup vars

--- qui.js
+++ qui.js
@@ -5,5 +5,17 @@ class QUIrenderer {
 		tmp.open("GET",url, false)
 		tmp.send("")
 		this.board=JSON.parse(tmp.responseText)
+
+		this.screenx=window.innerWidth //gets full window size
+		this.screeny=window.innerHeight
+
+		this.sizex=this.screenx/this.board["x"] //size of each individual box in grid
+		this.sizey=this.screeny/this.board["y"]
+
+		this.canv=document.getElementById("c")
+		this.disp=canv.getContext("2d")
+
+		disp.canvas.width=this.screenx //resizes canvas
+		disp.canvas.height=this.screeny
 	}
 }

c32a5e6cd6f1798eff3815212e81340934e3b536
dosisod <39638017+dosisod@users.noreply.github.com>
2018-11-05 22:24:35 -0800

    added basic text rendering

--- qui.js
+++ qui.js
@@ -21,7 +21,7 @@ class QUIrenderer {
 		this.mouse=function(e){
 			this.mousex=e.clientX
 			this.mousey=e.clientY
-			this.clicked()
+			this.action(this.clicked()["action"]) //runs JS code from clicked on grid
 		}
 
 		this.mouseh=this.mouse.bind(this) //mouse handler
@@ -31,50 +31,40 @@ class QUIrenderer {
 		this.disp.canvas.height=this.screeny
 	}
 	init() { //loads the board to the screen
-		if (this.board["bg"]["type"]=="color")
-			this.rect(this.board["bg"]["value"], 0,0,this.screenx,this.screeny)
+		if (this.board["bg"]["type"]=="color") {
+			this.rect(this.board["bg"]["value"], [0,0,this.screenx,this.screeny]) //draws background
+		}
 
-		for(var i of this.board["board"]) { //for each box in the board
-			this.rect(i["color"], ...this.grid(i["box"])) //draw outline
+		for(var i of this.board["board"]) { //for each box in the board:
+			this.rect(i["color"], this.grid(i["box"])) //draw outline
+			this.text(i["text"], this.grid(i["box"])) //render text
 		}
 	}
-	rect(color, x1, y1, x2, y2) {
+	rect(color, box) { //draws box of certain color at given pos
+		console.log(color, box)
 		this.disp.fillStyle=color
-		this.disp.fillRect(x1,y1,x2,y2)
+		this.disp.fillRect(box[0], box[1], box[2], box[3])
+	}
+	text(str, box) { //renders text at given pos
+		this.disp.font="48px monospace"
+		this.disp.fillStyle="black"
+		this.disp.fillText(str, box[0], box[1]+50)
 	}
 	action(s) { //runs JS code from string
 		var tmp=new Function(s)
 		return(tmp())
 	}
-	clicked() {
+	clicked() { //finds out what grid was clicked based off mouse pos
 		var tempx=~~(this.mousex/this.sizex)
 		var tempy=~~(this.mousey/this.sizey)
 		for (var k of this.board["board"]) {
 			if (tempx>=k["box"][0]&&tempx<k["box"][0]+k["box"][2]&&tempy>=k["box"][1]&&tempy<k["box"][1]+k["box"][3]) {
-				this.action(k["action"])
-				//return k;
+				//this.action(k["action"])
+				return k;
 			}
 		}
-
-		/*
-		for (var i=0;i<this.board["x"];i++) {
-			for (var j=0;j<this.board["y"];j++) {
-				//console.log(this.mousex>=i*this.sizex,this.mousex<(i+1)*this.sizex,this.mousey>j*this.sizey,this.mousey<(j+1)*this.sizey)
-				if (this.mousex>=i*this.sizex&&this.mousex<=(i+1)*this.sizex&&this.mousey>=j*this.sizey&&this.mousey<=(j+1)*this.sizey) {
-					alert("HERE I:"+j+" J:"+j)
-					for (var k of this.board["board"]) {
-						//if (tempx>=k["box"][0]&&tempx<k["box"][2]&&tempy<=k["box"][1]&&tempy<=k["box"][3]) {
-						if (i>=k["box"][0]&&i<k["box"][2]&&j<=k["box"][1]&&j<=k["box"][3]) {
-							this.action(k["action"])
-							//return k;
-						}
-					}
-				}
-			}
-		}*/
 	}
 	grid(box) { //returns cords for grid based on grid size and screen size
-		//alert([box[0]*this.screenx,box[1]*this.screeny,box[2]*this.screenx,box[3]*this.screeny])
 		return [box[0]*this.sizex,box[1]*this.sizey,box[2]*this.sizex,box[3]*this.sizey]
 	}
 }
